a1932c02051085c44a03b196b7428ed450f3896b
shmel9va <168944627+shmel9va@users.noreply.github.com>
2024-11-04 01:05:35 +0300

    Added multiplication, modulus, conjugate, inverse, and division operations for Quaternion

--- Quanterion.py
+++ Quanterion.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Quaternion:
     def __init__(self, w, x, y, z):
         """
@@ -43,3 +46,45 @@ class Quaternion:
             self.y - other.y,
             self.z - other.z
         )
+
+    def __mul__(self, other):
+        """
+        Операция умножения двух кватернионов.
+        """
+        w = self.w * other.w - self.x * other.x - self.y * other.y - self.z * other.z
+        x = self.w * other.x + self.x * other.w + self.y * other.z - self.z * other.y
+        y = self.w * other.y + self.y * other.w + self.z * other.x - self.x * other.z
+        z = self.w * other.z + self.x * other.y + self.z * other.w - self.y * other.x
+        return Quaternion(w, x, y, z)
+
+    def __abs__(self):
+        """
+        Вычисление модуля кватерниона.
+        """
+        return math.sqrt(self.w ** 2 + self.x ** 2 + self.y ** 2 + self.z ** 2)
+
+    def conjugate(self):
+        """
+        Вычисление сопряжения кватерниона.
+        """
+        return Quaternion(self.w, -self.x, -self.y, -self.z)
+
+    def inverse(self):
+        """
+        Вычисление обратного кватерниона.
+        """
+        mod_squared = abs(self) ** 2
+        conjugate = self.conjugate()
+        return Quaternion(
+            conjugate.w / mod_squared,
+            conjugate.x / mod_squared,
+            conjugate.y / mod_squared,
+            conjugate.z / mod_squared
+        )
+
+    def __truediv__(self, other):
+        """
+        Переопределение оператора деления для кватернионов.
+        Деление одного кватерниона на другой реализовано как умножение на обратный кватернион.
+        """
+        return self * other.inverse()
